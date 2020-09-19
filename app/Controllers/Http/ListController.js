2c2f40a1089dacac7aec60f17cab47aacad278fc
Ezekiel <iamwebwiz@gmail.com>
2020-09-19 06:45:55 +0100

    add characters object to controller method

--- app/Controllers/Http/ListController.js
+++ app/Controllers/Http/ListController.js
@@ -2,7 +2,20 @@
 
 class ListController {
   index({ request, response, view }) {
-    return view.render("welcome");
+    const characters = {
+      "Daenerys Targaryen": "Emilia Clarke",
+      "Jon Snow": "Kit Harington",
+      "Arya Stark": "Maisie Williams",
+      Melisandre: "Carice van Houten",
+      "Khal Drogo": "Jason Momoa",
+      "Tyrion Lannister": "Peter Dinklage",
+      "Ramsay Bolton": "Iwan Rheon",
+      "Petyr Baelish": "Aidan Gillen",
+      "Brienne of Tarth": "Gwendoline Christie",
+      "Lord Varys": "Conleth Hill",
+    };
+
+    return view.render("welcome", { characters });
   }
 }
 
