284c22aa3cec6c7a7f8a5cd88696020cee03571a
Ezekiel <iamwebwiz@gmail.com>
2020-09-19 06:25:02 +0100

    Update ListController

--- app/Controllers/Http/ListController.js
+++ app/Controllers/Http/ListController.js
@@ -1,9 +1,9 @@
-'use strict'
+"use strict";
 
 class ListController {
-  index({request, response, view}) {
-    return view.render('welcome')
+  index({ request, response, view }) {
+    return view.render("welcome");
   }
 }
 
-module.exports = ListController
+module.exports = ListController;
