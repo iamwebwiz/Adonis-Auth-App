4559bdfca7cde6c3e8be0883630b250113f8fbd7
Ezekiel <iamwebwiz@gmail.com>
2020-09-21 22:27:15 +0100

    update register controller

--- app/Controllers/Http/RegisterController.js
+++ app/Controllers/Http/RegisterController.js
@@ -1,6 +1,13 @@
-'use strict'
+"use strict";
 
 class RegisterController {
+  index({ request, response, view }) {
+    return view.render("auth.register");
+  }
+
+  register({ request, response }) {
+    //
+  }
 }
 
-module.exports = RegisterController
+module.exports = RegisterController;
