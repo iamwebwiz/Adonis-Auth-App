d6614375cc404a257fbc0ecc7fb306e0651027a8
Ezekiel <iamwebwiz@gmail.com>
2020-09-21 22:27:07 +0100

    update login controller

--- app/Controllers/Http/LoginController.js
+++ app/Controllers/Http/LoginController.js
@@ -1,6 +1,13 @@
-'use strict'
+"use strict";
 
 class LoginController {
+  index({ request, response, view }) {
+    return view.render("auth.login");
+  }
+
+  login({ request, response }) {
+    //
+  }
 }
 
-module.exports = LoginController
+module.exports = LoginController;
